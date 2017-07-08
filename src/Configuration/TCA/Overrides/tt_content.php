d42bc6be2a6d922f7ec0504dfa6c68ed7642adef
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2017-07-08 22:32:23 +0200

    trying to extend tt_content with one field, but skipping this one for now
    Signed-off-by: Alexander Hofbauer <hofbauer.alexander@gmail.com>

--- src/Configuration/TCA/Overrides/tt_content.php
+++ src/Configuration/TCA/Overrides/tt_content.php
@@ -16,6 +16,25 @@ $boot = function() {
         'fesearch_fesearch',
         'FILE:EXT:fesearch/Configuration/FlexForms/FesearchPlugin.xml'
         );
+
+    // Add "Exclude from Fesearch" Checkbox to all tt_contents
+    // $temporaryColumns = [
+    //     'tx_fesearch_excludeFromFesearch' => array(
+    //         'exclude' => 0,
+    //         'label' => 'LLL:EXT:fesearch/Resources/Private/Language/locallang.xlf:tt_content.excludeFromFesearch',
+    //         'config' => array(
+    //             'type' => 'check'
+    //         )
+    //     )
+    // ];
+    // \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+    //     'tt_content',
+    //     $temporaryColumns
+    // );
+    // \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    //     'tt_content',
+    //     'tx_fesearch_excludeFromFesearch'
+    // );
 };
 
 $boot();
---
+++ src/ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Modify tt_content table
+#
+-- CREATE TABLE tt_content (
+--     tx_fesearch_excludeFromFesearch tinyint(4) DEFAULT '0' NOT NULL
+-- );
